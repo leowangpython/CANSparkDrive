e998ec2c47bb5748456e94b9b6f1871d85f1255d
onebigunicorn <lwangx26@gmail.com>
2023-08-24 21:03:17 -0400

    git commit crime
    Co-authored-by: igor023 <igor024@users.noreply.github.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,27 +4,19 @@
 
 package frc.robot;
 
-import frc.robot.commands.DriveCommand;
 import frc.robot.commands.VisionCommand;
 
-import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.VisionSubsystem;
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 
 import org.photonvision.PhotonCamera;
 
 public class RobotContainer {
-  private final XboxController controller;
-  private final DoubleSupplier y;
-  private final DoubleSupplier x;
-  private final DriveSubsystem m_DriveSubsystem;
-  private final DriveCommand m_DriveCommand;
 
   private final NetworkTableInstance inst;
   private final PhotonCamera camera;
@@ -32,12 +24,6 @@ public class RobotContainer {
   private final VisionSubsystem m_VisionSubsystem;
 
   public RobotContainer() {
-    controller = new XboxController(0);
-    y = () -> controller.getLeftY();
-    x = () -> controller.getRightX();
-    m_DriveSubsystem = new DriveSubsystem();
-    m_DriveCommand = new DriveCommand(m_DriveSubsystem, x, y);
-    m_DriveSubsystem.setDefaultCommand(m_DriveCommand);
 
     inst = NetworkTableInstance.getDefault();
     inst.startServer();
@@ -49,7 +35,7 @@ public class RobotContainer {
   }
 
   public Command getAutonomousCommand() {
-    return m_DriveCommand;
+    return m_VisionCommand;
   }
 }
 
--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -4,9 +4,19 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+
+import java.util.List;
+
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
 import org.photonvision.targeting.PhotonTrackedTarget;
 
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.networktables.BooleanPublisher;
 import edu.wpi.first.networktables.DoublePublisher;
 
@@ -29,22 +39,31 @@ public class VisionSubsystem extends SubsystemBase {
         yaw = table.getDoubleTopic("yaw").publish();
         pitch = table.getDoubleTopic("pitch").publish();
         area = table.getDoubleTopic("area").publish();
-        skew = table.getDoubleTopic("skew").publish();
     }
 
     public void see(PhotonCamera camera) {
         var vision = camera.getLatestResult();
 
+        String field = AprilTagFields.k2023ChargedUp.m_resourceFile;
+        AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadFromResource(field);
+
         if (vision.hasTargets()) {
             hasTargets.set(true);
             PhotonTrackedTarget target = vision.getBestTarget();
-            yaw.set(target.getYaw());
-            pitch.set(target.getPitch());
-            area.set(target.getArea());
-            skew.set(target.getSkew());
+            
+            double distance = 0;
+            Rotation3d rotation = new Rotation3d();
+            Transform3d cameraToRobot = new Transform3d(new Translation3d(distance, rotation), rotation);
+            
+            Pose3d robotPose = PhotonUtils.estimateFieldToRobotAprilTag(target.getBestCameraToTarget(), aprilTagFieldLayout.getTagPose(target.getFiducialId()).get(), cameraToRobot);
+
+
         } 
         else {
             hasTargets.set(false);
         }
+
+        
     }
+        
 }
